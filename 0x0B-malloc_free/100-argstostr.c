1d522cba0f49bacfcd9cedd50d9bb3488838ab3d
Princeeze744 <trade2uwin@gmail.com>
2023-04-12 06:49:03 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,32 +11,32 @@
  */
 char *argstostr(int ac, char **av)
 {
-    int i, j, len = 0;
-    char *str;
+        int i, j, len = 0;
+        char *str;
 
-    if (ac == 0 || av == NULL)
-        return (NULL);
+        if (ac == 0 || av == NULL)
+                return (NULL);
 
-    /* Calculate the total length of the concatenated string */
-    for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j] != '\0'; j++)
-            len++;
-        len++; /* Account for space for newline character */
-    }
+        /* Calculate the total length of the concatenated string */
+        for (i = 0; i < ac; i++)
+        {
+                for (j = 0; av[i][j] != '\0'; j++)
+                        len++;
+                len++; /* Account for space for newline character */
+        }
 
-    str = malloc(sizeof(char) * (len + 1)); /* Add space for null terminator */
-    if (str == NULL)
-        return (NULL);
+        str = malloc(sizeof(char) * (len + 1)); /* Add space for null terminator */
+        if (str == NULL)
+                return (NULL);
 
-    /* Copy each argument into the concatenated string */
-    for (i = 0, len = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j] != '\0'; j++)
-            str[len++] = av[i][j];
-        str[len++] = '\n'; /* Add newline character */
-    }
-    str[len] = '\0'; /* Add null terminator */
+        /* Copy each argument into the concatenated string */
+        for (i = 0, len = 0; i < ac; i++)
+        {
+                for (j = 0; av[i][j] != '\0'; j++)
+                        str[len++] = av[i][j];
+                str[len++] = '\n'; /* Add newline character */
+        }
+        str[len] = '\0'; /* Add null terminator */
 
-    return (str);
+        return (str);
 }
