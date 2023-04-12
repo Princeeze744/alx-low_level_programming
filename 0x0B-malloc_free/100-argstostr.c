71187fa71928585e16706c471b1a60a9a446a300
Princeeze744 <trade2uwin@gmail.com>
2023-04-12 06:35:15 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,80 +1,42 @@
-
 #include "main.h"
-		
 #include <stdlib.h>
-		
+
 /**
-		
- * argstostr - main entry
-		
- * @ac: int input
-		
- * @av: double pointer array
-		
- * Return: 0
-		
+ * argstostr - Concatenates all the arguments in argv into a single string
+ *
+ * @ac: Number of arguments in argv
+ * @av: Array of arguments
+ *
+ * Return: A pointer to the concatenated string, or NULL on failure
  */
-		
 char *argstostr(int ac, char **av)
-		
 {
-		
-	int i, n, r = 0, l = 0;
-		
-	char *str;
-		
+    int i, j, len = 0;
+    char *str;
 
-		
-	if (ac == 0 || av == NULL)
-		
-		return (NULL);
-		
+    if (ac == 0 || av == NULL)
+        return (NULL);
 
-		
-	for (i = 0; i < ac; i++)
-		
-	{
-		
-		for (n = 0; av[i][n]; n++)
-		
-			l++;
-		
-	}
-		
-	l += ac;
-		
+    /* Calculate the total length of the concatenated string */
+    for (i = 0; i < ac; i++)
+    {
+        for (j = 0; av[i][j] != '\0'; j++)
+            len++;
+        len++; /* Account for space for newline character */
+    }
 
-		
-	str = malloc(sizeof(char) * l + 1);
-		
-	if (str == NULL)
-		
-		return (NULL);
-		
-	for (i = 0; i < ac; i++)
-		
-	{
-		
-	for (n = 0; av[i][n]; n++)
-		
-	{
-		
-		str[r] = av[i][n];
-		
-		r++;
-		
-	}
-		
-	if (str[r] == '\0')
-		
-	{
-		
-		str[r++] = '\n';
-		
-	}
-		
-	}
-		
-	return (str);
-		
+    str = malloc(sizeof(char) * len);
+    if (str == NULL)
+        return (NULL);
+
+    /* Copy each argument into the concatenated string */
+    for (i = 0, len = 0; i < ac; i++)
+    {
+        for (j = 0; av[i][j] != '\0'; j++)
+            str[len++] = av[i][j];
+        str[len++] = '\n'; /* Add newline character */
+    }
+    str[len] = '\0'; /* Add null terminator */
+
+    return (str);
 }
