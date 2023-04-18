494d0ad1afead4c6fc3e1ca1ce54fc8d4207d3c6
Princeeze744 <trade2uwin@gmail.com>
2023-04-17 23:14:26 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,47 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A dog's basic information
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Description: This struct contains basic information about a dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+/**
+ * _strcpy - Copies a string from source to destination.
+ * @dest: The destination string.
+ * @src: The source string.
+ *
+ * Return: A pointer to the destination string.
+ */
+char *_strcpy(char *dest, char *src);
+
+/**
+ * _strlen - Calculates the length of a string.
+ * @s: The input string.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s);
+
+#endif
+
