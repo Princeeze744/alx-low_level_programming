66030d6d28f7d67874ff5e7ac2bf12eb3664e492
princeeze744 <trade2uwin@gmail.com>
2023-04-27 14:25:52 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include "lists.h"
+
 /**
  * print_list - Prints all the elements of a linked list.
  *
@@ -7,20 +10,20 @@
  */
 size_t print_list(const list_t *h)
 {
-        size_t count = 0;
+	size_t s = 0;
 
-        while (h != NULL)
-        {
-                if (h->str == NULL)
-                        printf("[0] (nil)\n");
-                else
-                        printf("[%u] %s\n", h->len, h->str);
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
 
-                h = h->next;
-                count++;
-        }
+		h = h->next;
+		s++;
+	}
 
-        return (count);
+	return (s);
 }
 
 /*
@@ -33,5 +36,53 @@ size_t print_list(const list_t *h)
  *
  * Return:
  * 	size_t: The number of nodes printed.
- */ 
+ */
 
+int main(void)
+{
+	/* create a linked list */
+	list_t *head = NULL;
+	list_t *second = NULL;
+	list_t *third = NULL;
+
+	head = malloc(sizeof(list_t));
+	if (head == NULL)
+		return (1);
+	head->str = "Hello";
+	head->len = 5;
+	head->next = NULL;
+
+	second = malloc(sizeof(list_t));
+	if (second == NULL)
+	{
+		free(head);
+		return (1);
+	}
+	second->str = "World";
+	second->len = 5;
+	second->next = NULL;
+	head->next = second;
+
+	third = malloc(sizeof(list_t));
+	if (third == NULL)
+	{
+		free(head);
+		free(second);
+		return (1);
+	}
+	third->str = "!";
+	third->len = 1;
+	third->next = NULL;
+	second->next = third;
+
+	/* print the linked list */
+	size_t node_count = print_list(head);
+	printf("Number of nodes: %lu\n", node_count);
+
+	/* free the linked list */
+	free(third);
+	free(second);
+	free(head);
+
+	return (0);
+}
