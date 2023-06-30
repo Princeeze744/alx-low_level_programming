9914a1427e962ec8ab918a356575720b7db6e2a4
Princeeze744 <Trade2uwin@gmail.com>
2023-06-30 13:50:37 +0100

    Task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,10 +11,10 @@ size_t print_dlistint(const dlistint_t *head)
 
 	count = 0;
 
-	while (h)
+	while (head)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
+		printf("%d\n", head->n);
+		head = head->next;
 		count++;
 	}
 	return (count);
